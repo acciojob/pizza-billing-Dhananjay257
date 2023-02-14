31067fc650273b500e1c546a8082a0eaa4f96b5c
Dhananjay-Dubey <dhananjaydhanu505@gmail.com>
2023-02-14 12:01:57 +0530

    First

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,11 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
+static int sum;
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        addExtraCheese();
+        addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,66 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
     private Boolean isVeg;
     private String bill;
 
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            this.price+=300;
+        }
+        else
+        {
+            this.price+=400;
+        }
+        System.out.println("Base Price Of The Pizza: "+this.price);
     }
 
     public int getPrice(){
         return this.price;
     }
 
+    boolean vis=true;
     public void addExtraCheese(){
         // your code goes here
+        if(vis)
+        {
+            this.price+=80;
+            System.out.println("Extra Cheese Added: 80");
+            vis=false;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg)
+        {
+            this.price+=70;
+            System.out.println("Extra Toppings Added: 70");
+        }
+        else
+        {
+            this.price+=120;
+            System.out.println("Extra Toppings Added: 120");
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price+=20;
+        System.out.println("Paperbag Added: 20");
     }
 
     public String getBill(){
         // your code goes here
+        this.bill=String.valueOf(this.price);
+        System.out.println("Total Price: "+this.bill);
         return this.bill;
     }
 }
